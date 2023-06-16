09fd4bca4c02f24bf82e3a59abf8b03c7f77d51e
Kauan Gomes <Kauang13@gmail.com>
2023-06-15 21:07:58 -0300

    correções

--- js/RequisicaoAPI.js
+++ js/RequisicaoAPI.js
@@ -1,3 +1,19 @@
 //62f0613f0cd845b6a9eb49f41db94d82
 
 //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=62f0613f0cd845b6a9eb49f41db94d82
+
+//https://api.openweathermap.org/data/2.5/weather?id=524901&lang=fr&appid=62f0613f0cd845b6a9eb49f41db94d82
+
+
+
+const requisicaoClima = async (evento) => {
+    event.preventDefault()
+    
+    try{
+        const response = await fetch (``)
+
+    }catch (error){
+        alert(error)
+    }
+    
+}
--- js/main.js
+++ js/main.js
@@ -1,30 +1,41 @@
 const btn__adicionar = document.querySelector('.adicionar')
+const btn__close = document.querySelector('#close')
 
-console.log(btn__adicionar)
 
 //colocando função de click no botao
-btn__adicionar.addEventListener('click', abrirModal)  
+btn__adicionar.addEventListener('click', abrirModal)
 
 //função para Abrir o Modal
 function abrirModal() {
-    const ModalCompleto = document.querySelector('.ParteDeForaModal')
-    const styleAtual = ModalCompleto.style.display
+  const ModalCompleto = document.querySelector('.ParteDeForaModal')
+  const styleAtual = ModalCompleto.style.display
 
-    if(styleAtual == 'block'){
-        ModalCompleto.style.display = 'none'
-    }
-    else{
-        ModalCompleto.style.display = 'block'
-    }
+  if (styleAtual == 'block') {
+    ModalCompleto.style.display = 'none'
+  }
+  else {
+    ModalCompleto.style.display = 'block'
+  }
 }
 
-window.onclick = function(event) {
-    const modal = document.querySelector('.ParteDeForaModal')
+// fechando o modal quando se clica na parte de fora dele
+window.onclick = function (event) {
+  const modal = document.querySelector('.ParteDeForaModal')
   if (event.target == modal) {
     abrirModal()
   }
 }
 
+//fechando o modal pelo botão X
+btn__close.addEventListener('click', evento => {
+  console.log(evento.target.textContent)
+  const modal = document.querySelector('.ParteDeForaModal')
+  console.log(modal)
+  if(evento.target.textContent == 'X'){
+    modal.style.display = 'none'
+  }
+
+})
 
 
 
